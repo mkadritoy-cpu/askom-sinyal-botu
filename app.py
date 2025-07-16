ce595f38283fcb2d77f1aced6ab0ea5a05c2d996
mkadritoy-cpu <mkadritoy@gmail.com>
2025-07-16 13:35:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,29 @@ import requests
 app = Flask(__name__)
 
 # Bot tokenin
-TOKEN = "7147929892:AAHkXxvAfmrtW3z7YHEEDtE9Yk8xYVgsQpk"
+TOKEN = "7147929892:AAHkXxxvAfmrtl8z7YHEEDtE9Yk8xYVesQk"
 TELEGRAM_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     print("🔵 Gelen veri:", data)
-    
-    if 'message' in data:
-        chat_id = data['message']['chat']['id']
-        text = data['message'].get('text', '')
-        
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
         print("🟡 Chat ID:", chat_id)
         print("🟡 Text:", text)
-        
-         Yanıt mesajı
-        reply = "Bot çalışıyor aşkom 💖"
-        
+
+          Yanıt mesajı
+        reply = "Bot çalışıyor aşkom ❤️"
         payload = {
             "chat_id": chat_id,
             "text": reply
         }
-        
+
         response = requests.post(TELEGRAM_URL, json=payload)
         print("🟢 Telegram yanıtı:", response.status_code, response.text)
-        
+
     return jsonify({"ok": True})
